ec5ad638c82487a45da8b79a0612d906fc826b2e
[nelson] <[nelsoncastro@hotmail.com]>
2020-01-07 14:44:06 -0300

    ahi q poner header y footer reutilizar register y login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,7 +26,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo ='almacen/articulo';
 
     /**
      * Create a new controller instance.
